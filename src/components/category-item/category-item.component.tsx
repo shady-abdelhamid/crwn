28293e4a424b388cde674e8bfea164632fbb32d6
Shady Abdelhamid <shady.abdelhamid@capiter.net>
2022-09-24 16:24:50 +0200

    feat: #2 add directory and category components

---
+++ src/components/category-item/category-item.component.tsx
@@ -0,0 +1,31 @@
+import "./category-item.styles.scss";
+
+type Category = {
+  id: number;
+  title: string;
+  imageUrl: string;
+};
+
+interface Props {
+  category: Category;
+}
+
+const CategoryItem = ({ category }: Props) => {
+  const { title, imageUrl } = category;
+  return (
+    <div className="category">
+      <div
+        className="category__img"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category__body">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;

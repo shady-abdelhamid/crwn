6f3ab5bb5d0dc57ebd1e122457d1ba9371ecbaab
Shady Abdelhamid <shady.abdelhamid@capiter.net>
2022-09-28 13:27:13 +0200

    feat: #4 add google redirect authentication

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,7 +1,23 @@
-import { Fragment } from "react";
-import { createUserDocumentFromAuth, signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import { getRedirectResult } from "firebase/auth";
+import { Fragment, useEffect } from "react";
+import {
+  auth,
+  createUserDocumentFromAuth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  useEffect(() => {
+    const redirectAuth = async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = createUserDocumentFromAuth(response.user);
+      }
+    };
+    redirectAuth();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = createUserDocumentFromAuth(user);
@@ -13,6 +29,9 @@ const SignIn = () => {
         <h1>Sign in page</h1>
       </div>
       <button onClick={logGoogleUser}>Sign in with google popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with google redirect
+      </button>
     </Fragment>
   );
 };

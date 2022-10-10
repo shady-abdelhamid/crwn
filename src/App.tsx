3eda78523c70d5ba558056a9e35a26750254ed5e
Shady Abdelhamid <shady.abdelhamid@life-house.com>
2022-10-10 14:38:21 +0200

    feat: add sign in form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component"
-import SignIn from "./routes/sign-in/sign-in.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Navigation />}>
       <Route index element={<Home />} />
-      <Route path="sign-in" element={<SignIn />} />
-
+      <Route path="auth" element={<Authentication />} />
+      <Route path="*" element={<Home />} />
     </Route>
   </Routes>
 );

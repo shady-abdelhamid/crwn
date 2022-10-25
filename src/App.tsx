5dc116f5dc23e8b47f2e12a1180523219c61a81b
Shady Abdelhamid <shady.abdelhamid@life-house.com>
2022-10-25 16:44:14 +0200

    feat: #10 add checkout page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./routes/home/home";
 import Navigation from "./routes/navigation/navigation";
 import Authentication from "./routes/authentication/authentication";
 import Shop from "./routes/shop/shop";
+import Checkout from "./routes/checkout/checkout";
 
 const App = () => (
   <Routes>
@@ -10,6 +11,7 @@ const App = () => (
       <Route index element={<Home />} />,
       <Route path="shop" element={<Shop />}></Route>
       <Route path="auth" element={<Authentication />} />
+      <Route path="checkout" element={<Checkout />} />
       <Route path="*" element={<Home />} />
     </Route>
   </Routes>
